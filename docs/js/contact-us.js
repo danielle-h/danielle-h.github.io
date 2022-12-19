ff14ffd0534c8444f49642136d01aef774e85693
danielle <dsavir.h@gmail.com>
2022-12-19 20:36:32 +0200

    feat(contact): changed form submission to javascript

--- docs/js/contact-us.js
+++ docs/js/contact-us.js
@@ -1,7 +1,39 @@
-const form = document.getElementById('form');
-const result = document.getElementById('result');
+const form = document.getElementById('contact-form');
+const result = document.getElementById('fomr-result');
 
 form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    result.innerHTML = "Thank you! I will get back to as soon as possible";
-  });
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+                result.classList.add("notice-success");
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
